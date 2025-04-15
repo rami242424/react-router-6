b4d47b5f6a4a874022ec42367dacf933eb0e2a01
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-15 16:10:21 +0900

    4.8 Extras such as useSearchParams, useOutletContext

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { users } from "../db";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Home() {
-   return <div>
-    <h1>Users</h1>
-    <ul>
-        {users.map((user) => (
-            <li key={user.id}>
-                <Link to={`/users/${user.id}`}>
-                    {user.name}
-                </Link>
-            </li>
-        ))}
-    </ul>
-   </div>
+    const [readSearchParams, setSearchParams] = useSearchParams();
+    console.log(readSearchParams);
+   return (
+    <div>
+            <h1>Users</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        <Link to={`/users/${user.id}`}>
+                            {user.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+    </div>
+   );
 }
 export default Home;
